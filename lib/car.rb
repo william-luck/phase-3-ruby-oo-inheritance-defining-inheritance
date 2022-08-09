fdf78a678cb5488538f0ac081d8b32ae72ca3aff
William Luck <luck.william.h@gmail.com>
2022-08-09 11:08:05 -0500

    commit tree 75249c9bba634b6ce47d77c509ff4e1d1de807cb

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+# Subclass of vehicle
+
+class Car < Vehicle
+    # This already exists in the parent vechile class, but we are overwriting it here.
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
